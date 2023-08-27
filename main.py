9509dde05766d2c368db869a07a99c12deacaf7b
michellemwangi01 <michellemwangim@gmail.com>
2023-08-28 00:07:42 +0300

    main.py cleanup

--- main.py
+++ main.py
@@ -3,15 +3,14 @@ from restaurant import Restaurant
 from review import Review
 
 print("\n------------------------------------- CUSTOMERS ------------------------------------------")
-customer1 = Customer("Mirah","Samirah")
-customer2 = Customer("Samantha","Mariah")
-customer3 = Customer("John","Doe")
-customer4 = Customer("John","Smith")
+customer1 = Customer("Mirah", "Samirah")
+customer2 = Customer("Samantha", "Mariah")
+customer3 = Customer("John", "Doe")
+customer4 = Customer("John", "Smith")
 print(customer4.full_name)
 print(customer3.full_name)
 print(customer2.full_name)
 print(customer1.full_name)
-
 
 print("\n----------------------------------- RESTAURANTS ------------------------------------------")
 restaurant1 = Restaurant("Hibbs")
@@ -20,7 +19,6 @@ restaurant3 = Restaurant("Quickmart")
 print(restaurant3.name)
 print(restaurant2.name)
 print(restaurant1.name)
-
 
 print("\n--------------------------------- REVIEWS ------------------------------------------------")
 review1 = Review(customer1, restaurant1, 10)
@@ -31,22 +29,20 @@ review5 = Review(customer2, restaurant2, 1)
 review6 = Review(customer2, restaurant2, 18)
 print(f'List of reviews({len(Review.REVIEWS)}): \n {Review.REVIEWS}')
 
-
-
-print("\n-------------------------------restaurant add_review()----------------------------------")
+print("\n------------------------------- restaurant add_review() ----------------------------------")
 restaurant1.add_review([review1, review2])
 restaurant2.add_review([review3, review4, review5, review6])
-print(f"List of '{restaurant1.name}' reviews({len(restaurant1.restaurant_reviews)}): \n {restaurant1.restaurant_reviews}")
+print(
+    f"List of '{restaurant1.name}' reviews({len(restaurant1.restaurant_reviews)}): \n {restaurant1.restaurant_reviews}")
 
-
-print("\n-------------------------------customer add_review()-------------------------------------")
+print("\n------------------------------- customer add_review() -------------------------------------")
 customer1.add_review(restaurant3, 23)
 print(f"Reviews by {customer1.full_name}:\n {customer1.reviews()}")
 
-print("\n-------------------------------restaurant restaurant_reviews()---------------------------")
+print("\n------------------------------- restaurant restaurant_reviews() ---------------------------")
 print(f"{restaurant2.name}'s reviews:\n{restaurant2.restaurant_reviews}")
 
-print("\n-------------------------------restaurant customers reviews------------------------------")
+print("\n------------------------------- restaurant customers reviews ------------------------------")
 print(restaurant2.customers_with_reviews())
 
 print("\n------------------------------- customer reviews-----------------------------------------")
